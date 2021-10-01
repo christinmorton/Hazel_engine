08edce3f8d62b8b6fbf298885c4c821d38102bc1
Christin Morton <sysadmin@christinmorton.com>
2021-10-01 15:47:40 -0700

    setup premake and spdlog library

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,12 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!"); 
+	int a = 5;
+	HZ_INFO("Client says hello Var={0} times!", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
